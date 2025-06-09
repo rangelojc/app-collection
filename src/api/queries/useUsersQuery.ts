c7d84936283c636eea156b8b69942a2e82cd750b
Angelo <ramuzuconcepcion@gmail.com>
2025-06-09 20:51:32 +0800

    remove unused files

--- src/api/queries/useUsersQuery.ts
+++ src/api/queries/useUsersQuery.ts
@@ -2,12 +2,10 @@ import { baseUrl, customFetch, throwUnhandledApiError } from "@/api/functions";
 import { ListResponse, PaginationRequest } from "@/api/types";
 import useCustomQuery from "@/api/useCustomQuery";
 import { createPaginationRequest } from "@/functions/factories";
-import { useLogoutOnForbidden } from "@/hooks/useAuth";
 
 export const USERS_QUERY_KEY = "users" as const;
 
 const useUsersQuery = (payload: PaginationRequest) => {
-  const logoutOnForbidden = useLogoutOnForbidden();
   const { query } = createPaginationRequest(payload);
 
   return useCustomQuery<ListResponse<any[]>>([USERS_QUERY_KEY], async () => {
@@ -19,11 +17,6 @@ const useUsersQuery = (payload: PaginationRequest) => {
     if (rsp.status === 200) {
       const data = await rsp.json();
       return data;
-    }
-
-    if (rsp.status === 401) {
-      logoutOnForbidden();
-      return;
     }
 
     return throwUnhandledApiError(rsp);
