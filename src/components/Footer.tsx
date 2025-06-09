2ec0b90277ac7d1ca556cfff95262a4ceecb0d3c
Angelo <ramuzuconcepcion@gmail.com>
2025-06-09 21:01:50 +0800

    fix: update app name and footer text to 'APP COLLECTION'

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
 
   return (
     <footer className="bg-transparent dark:bg-muted/20 w-full h-[var(--footer-height)] flex-row-center text-xs px-2 text-neutral-500 border-none">
-      &copy; {new Date().getFullYear()} {t("footer.msg")}
+      &copy; {new Date().getFullYear()} APP COLLECTION
     </footer>
   );
 }
